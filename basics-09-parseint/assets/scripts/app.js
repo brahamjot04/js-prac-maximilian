8703f19c889751d6f3092448b5cd30ac5378cb94
Brahamjot Singh <crownbrahamjot2004@gmail.com>
2024-01-17 22:53:42 +0530

    updated

---
+++ basics-09-parseint/assets/scripts/app.js
@@ -0,0 +1,10 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function add(num1,num2) {
+  const result = num1 + num2;
+}
+
+addBtn.addEventListener('click', add);
+
+outputResult(currentResult, calculationDescription);
